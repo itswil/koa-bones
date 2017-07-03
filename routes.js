c966b305cc84e9751637e4dacae4d1f083c8caeb
Wil W <ww8668@gmail.com>
2017-07-03 22:36:10 +0100

    Fix non-returning routes

--- routes.js
+++ routes.js
@@ -4,10 +4,10 @@ const router = KoaRouter()
 
 router
   .get('/', async (ctx, next) => {
-    ctx.render('views/homepage')
+    return ctx.render('views/homepage')
   })
   .get('/about', async (ctx, next) => {
-    ctx.render('views/about')
+    return ctx.render('views/about')
   })
 
 module.exports = router
