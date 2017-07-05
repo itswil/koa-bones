617686ece2a9f4b0643de0ec180d88ca4130ff4a
Wil W <ww8668@gmail.com>
2017-07-05 22:57:34 +0100

    Add connection and save to DB

--- api.js
+++ api.js
@@ -1,10 +1,22 @@
 const KoaRouter = require('koa-router')
 const KoaBody = require('koa-body')
+const mongoose = require('mongoose')
 
-const api = KoaRouter()
+const CONFIG = require('./config')
+
+const router = KoaRouter()
 const body = KoaBody()
+mongoose.connect(CONFIG.mongoPath + CONFIG.databaseName, { useMongoClient: true })
+mongoose.Promise = global.Promise
 
-api
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String
+})
+
+const User = mongoose.model('User', userSchema)
+
+router
   .get('/api/employees', async (ctx, next) => {
     const response = {
       employees: [
@@ -14,5 +26,14 @@ api
     }
     ctx.body = response
   })
+  .get('/api/dbsave', async (ctx, next) => {
+    const user = new User({
+      name: 'John',
+      email: 'john@example.com'
+    })
 
-module.exports = api
+    await user.save()
+    ctx.body = 'OK'
+  })
+
+module.exports = router
